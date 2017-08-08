d26d45fae8e9d4b1208d429b0f821be68843e40b
lukas <lukasz@enginearch.com>
2017-08-08 10:05:22 +0200

    added new cap task

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,6 +25,35 @@ namespace :db do
   end
 end
 
+namespace :assets do
+  desc 'Precompile assets locally and then rsync to web servers'
+  task :precompile do
+    run_locally do
+      with rails_env: stage_of_env do
+        execute :bundle, 'exec rake assets:precompile'
+      end
+    end
+
+    on roles(:web), in: :parallel do |server|
+      run_locally do
+        execute :rsync,
+          "-a --delete ./public/packs/ #{fetch(:user)}@#{server.hostname}:#{shared_path}/public/packs/"
+        execute :rsync,
+          "-a --delete ./public/assets/ #{fetch(:user)}@#{server.hostname}:#{shared_path}/public/assets/"
+      end
+    end
+
+    run_locally do
+      execute :rm, '-rf public/assets'
+      execute :rm, '-rf public/packs'
+    end
+  end
+end
+
+namespace :deploy do
+  after :updated, 'assets:precompile'
+end
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
