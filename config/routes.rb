779b1df2fc66074db6687ae9024cc1dfed3abfa0
lukas <lukasz@enginearch.com>
2017-08-09 12:39:49 +0200

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_page#home'
   get '/profile', to: 'static_page#profile'
+  get '/profile-edit', to: 'static_page#profile-edit'
 end
