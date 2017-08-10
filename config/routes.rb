5530a09ec948a99ea303c91b824ad6ccdd882351
lukas <lukasz@enginearch.com>
2017-08-10 21:59:21 +0200

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get '/profile-edit', to: 'static_page#profile-edit'
   get '/profile-gallery', to: 'static_page#profile-gallery'
 
+  get '/hotline', to: 'static_page#hotline'
+
   get '/layout', to: 'static_page#layout'
 end
